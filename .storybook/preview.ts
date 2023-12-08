53f0b43f42ccdd6166791d548c84dce835f5d1ba
Bajra Manandhar <manadhar.bajra24@gmail.com>
2023-12-08 14:10:45 +0545

    initial commit

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from "@storybook/react";
+import DocumentationTemplate from "./DocumentationTemplate.mdx";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      toc: {
+        contentsSelector: '.sbdocs-content',
+        headingSelector: 'h1, h2, h3',
+        ignoreSelector: '#primary',
+        title: 'Table of Contents',
+        disable: false,
+        unsafeTocbotOptions: {
+          orderedList: false,
+        },
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Components/Common/Button',
+  tags: ['autodocs'],
+  args: {
+    label: 'Hello World Button',
+  },
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta
+
+type Story = StoryObj<typeof Button>;
+
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+  },
+};
+
+export const Secondary: Story = {};
+
+export const RedButtonLarge: Story =  {
+  args: {
+    size: 'large',
+  }
+}
+
+RedButtonLarge.argTypes = {
+  backgroundColor: { control: 'inline-radio', options: ['red', 'blue'] },
+}
